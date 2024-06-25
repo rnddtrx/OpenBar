fa2ca986a7ea26decf42ebb79be0b431a90bd7fb
rems99 <35024805+rems99@users.noreply.github.com>
2024-06-25 17:51:52 +0200

    OpenBar with Personal Info

--- src/main/java/be/ipam/openbar/controller/PersonalInfoController.java
+++ src/main/java/be/ipam/openbar/controller/PersonalInfoController.java
@@ -3,11 +3,11 @@ package be.ipam.openbar.controller;
 import be.ipam.openbar.dto.AppUserDto;
 import be.ipam.openbar.service.PersonalInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import be.ipam.openbar.dto.PersonalInfoDto;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/personalinfo")
@@ -15,9 +15,17 @@ public class PersonalInfoController {
     @Autowired
     PersonalInfoService personalInfoService;
     // get all personal by user id
-    @RequestMapping("/personalinfos")
-    public List<PersonalInfoDto> getAllPersonalInfos(AppUserDto appuser){
-        return personalInfoService.getAllPersonalInfos(appuser);
+    @GetMapping("/personalinfos")
+    public List<PersonalInfoDto> getAllPersonalInfos(@RequestBody  AppUserDto appUser){
+        return personalInfoService.getAllPersonalInfos(appUser);
     }
-
+    //save personal info
+    @PostMapping("/personalinfo")
+    public PersonalInfoDto savePersonalInfo(@RequestBody PersonalInfoDto personalInfoDto) {
+        return personalInfoService.savePersonalInfo(personalInfoDto);
+    }
+    @GetMapping("/personalinfo")
+    public PersonalInfoDto getPersonalInfoById(@RequestParam UUID id) {
+        return personalInfoService.getPersonalInfoById(id);
+    }
 }
--- src/main/java/be/ipam/openbar/service/PersonalInfoService.java
+++ src/main/java/be/ipam/openbar/service/PersonalInfoService.java
@@ -3,11 +3,13 @@ package be.ipam.openbar.service;
 import be.ipam.openbar.dto.AppUserDto;
 import be.ipam.openbar.dto.PersonalInfoDto;
 import be.ipam.openbar.mapper.AppUserMapper;
+import be.ipam.openbar.mapper.PersonalInfoMapper;
 import be.ipam.openbar.repository.PersonalInfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PersonalInfoService {
@@ -15,9 +17,20 @@ public class PersonalInfoService {
     private PersonalInfoRepository personalInfoRepository;
     @Autowired
     private AppUserMapper appUserMapper;
+    @Autowired
+    private PersonalInfoMapper personalInfoMapper;
 
     // get all personal by user id
     public List<PersonalInfoDto> getAllPersonalInfos(AppUserDto appuser){
         return personalInfoRepository.findByAppUser(appUserMapper.toEntity(appuser));
+    }
+
+    //save personal info
+    public PersonalInfoDto savePersonalInfo(PersonalInfoDto personalInfoDto) {
+        return personalInfoMapper.toDto(personalInfoRepository.save(personalInfoMapper.toEntity(personalInfoDto)));
+    }
+
+    public PersonalInfoDto getPersonalInfoById(UUID id) {
+        return personalInfoMapper.toDto(personalInfoRepository.findById(id).orElse(null));
     }
 }
